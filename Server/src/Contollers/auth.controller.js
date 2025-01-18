6aaf8be2fffe0092f524a3b66db77eade194af8c
unknown <saarthakagarwal0408@gmail.com>
2025-01-19 00:10:15 +0530

    Added Login functionality in the backend code

--- Server/src/Contollers/auth.controller.js
+++ Server/src/Contollers/auth.controller.js
@@ -1,4 +1,13 @@
 import User from "../Models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
+
+function generateOTP() {
+    // Generate number between 100000 and 999999
+    return Math.floor(100000 + Math.random() * 900000);
+}
 
 export const signup = async (req, res) => {
     try {
@@ -18,13 +27,14 @@ export const signup = async (req, res) => {
             return res.status(400).json({ error: "User with same email already exists" });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = new User({
             userName,
             email,
-            password,
+            password: hashedPassword,
         });
-
-        console.log(newUser);
 
         await newUser.save();
 
@@ -36,6 +46,36 @@ export const signup = async (req, res) => {
 
     } catch (error) {
         console.log("Error in signup functionality");
+        return res.status(500).json({error : "Internal Server problem"})
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({error : "Please fill all the fields"})
+        }
+
+        const user = await User.findOne({email});
+        
+        if(!user){
+            return res.status(404).json({error : "User not found"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(isPasswordCorrect === false){
+            return res.status(401).json({error : "Invalid credentials"})
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.MY_SECRET, { expiresIn: '1h' });
+        return res.status(200).json({
+            message : "Login successful",
+            token
+        })
+    } catch (error) {
+        console.log("Error in login functionality");
         return res.status(500).json({error : "Internal Server problem"})
     }
 }
--- Server/src/Routes/auth.routes.js
+++ Server/src/Routes/auth.routes.js
@@ -1,10 +1,9 @@
-import { signup } from "../Contollers/auth.controller.js";
+import { login, signup } from "../Contollers/auth.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup", signup)
-// router.post("/login",()=>{
-// })
+router.post("/login", login)
 
 export default router
