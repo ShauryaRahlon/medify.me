2e545ac76dba83d578b4e04c960a639ea7ffb53a
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-01-18 14:55:53 +0530

    Changing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -9,20 +9,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1> </h1>
       <div className="card">
-        <button className='bg-blue-500 border-blue-500 hover:bg-blue-700 hover:border-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
