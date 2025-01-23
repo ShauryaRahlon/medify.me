c08cc1b6408ec87db3e737b7755fddb45f25918c
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-01-23 23:45:24 +0530

    Final Deployment

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://medify-me-ed85.vercel.app/',  // Allow frontend origin
+    origin: 'https://medify-me.vercel.app/',  // Allow frontend origin
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
 }));
