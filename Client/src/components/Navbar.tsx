5ca352c92a534d3c1106562a6a635db080a8d58c
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-01-24 00:19:31 +0530

    Deployment Successfull

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { Menu, X } from "lucide-react";
 import AuthModal from "./AuthModal";
 
